3c69b725657ca86d54b184648244dea3a7a2bfe6
aGiaa91 <marija.stojiljkovic.gia@gmail.com>
2019-01-14 12:08:50 +0100

    dodavanje kolone jmbg i ubacivanje kao password-a

---
+++ backend/views/user/_formUpdate.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\controllers\RollController;
+use backend\models\Roll;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\User */
+/* @var $form yii\widgets\ActiveForm */
+
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'roll_id')->dropDownList(ArrayHelper::map(Roll::find()->all(), 'id', 'title')) ?>
+
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
