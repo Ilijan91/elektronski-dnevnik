708da58a8a82d0cea2f3e780d2fa5d6f1fb6711e
aGiaa91 <marija.stojiljkovic.gia@gmail.com>
2019-01-15 10:29:17 +0100

    pravljenje modela i crud operacija za tabele subject i student

---
+++ backend/models/Student.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "student".
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property int $JMBG
+ * @property string $address
+ * @property string $phone
+ * @property int $user_id
+ * @property int $department_id
+ *
+ * @property Diary[] $diaries
+ * @property User $user
+ * @property Department $department
+ */
+class Student extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'student';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['first_name', 'last_name', 'JMBG', 'address', 'phone', 'user_id', 'department_id'], 'required'],
+            [['JMBG', 'user_id', 'department_id'], 'integer'],
+            [['first_name', 'last_name', 'address', 'phone'], 'string', 'max' => 255],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [['department_id'], 'exist', 'skipOnError' => true, 'targetClass' => Department::className(), 'targetAttribute' => ['department_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'JMBG' => 'Jmbg',
+            'address' => 'Address',
+            'phone' => 'Phone',
+            'user_id' => 'User ID',
+            'department_id' => 'Department ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDiaries()
+    {
+        return $this->hasMany(Diary::className(), ['student_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDepartment()
+    {
+        return $this->hasOne(Department::className(), ['id' => 'department_id']);
+    }
+}
---
+++ backend/views/student/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Student */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="student-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'JMBG')->textInput() ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'user_id')->textInput() ?>
+
+    <?= $form->field($model, 'department_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
