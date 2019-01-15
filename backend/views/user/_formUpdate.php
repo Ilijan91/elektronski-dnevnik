53f778fb9b5c07a698c30ef87d41b9aa748e6fb9
Tamara <tttamarita@yahoo.com>
2019-01-15 10:35:18 +0100

    update

--- backend/views/user/_formUpdate.php
+++ backend/views/user/_formUpdate.php
@@ -24,7 +24,9 @@ use backend\models\Roll;
 
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true,]) ?>
+
+    <?= $form->field($model, 'JMBG')->textInput(['maxlength' => true,'readonly'=>true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
