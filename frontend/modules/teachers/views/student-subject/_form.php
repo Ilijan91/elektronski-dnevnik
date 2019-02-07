1b598836af6abcb06036b15b42917c537c1d9839
Kristina2103 <kristinapejcic1994@gmail.com>
2019-02-07 09:38:38 +0100

    index i news izmene

---
+++ frontend/modules/teachers/views/student-subject/_form.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use backend\models\Student;
+use backend\models\Subject;
+use yii\helpers\ArrayHelper;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\modules\Teacher\models\StudentSubject */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="student-subject-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'student_id')->dropDownList(ArrayHelper::map($modelStudents, 'id', 'full_name'), ['prompt' => 'Select student']) ?>
+
+    <?= $form->field($model, 'subject_id')->dropDownList(ArrayHelper::map(Subject::find()->select(['id', 'title'])->all(), 'id', 'title'), ['prompt' => 'Select subject']) ?>
+
+    <?= $form->field($model, 'grade')->textInput() ?>
+
+    <?= $form->field($model, 'final_grade')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
