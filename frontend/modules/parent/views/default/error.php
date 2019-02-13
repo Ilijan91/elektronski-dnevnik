1a081bf7845ad5cae6e1e65c9fc5df0189ddf7d2
aGiaa91 <marija.stojiljkovic.gia@gmail.com>
2019-02-13 09:56:44 +0100

    error

---
+++ frontend/modules/parent/views/default/error.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+?>
+<div class="site-error">
+
+    <h1>Error!</h1>
+
+    <div class="alert alert-danger">
+    <h3>There is no data to show!</h3>
+    </div>
+
+</div>
